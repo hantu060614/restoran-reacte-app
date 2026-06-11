eb05e4e6dccffeb23bef445849d4da59090ea275
hantu060614 <hantu060614@gmail.com>
2026-06-11 12:07:47 +0700

    Updates

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,22 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+        tabBarActiveTintColor: "#f97316",
+        tabBarInactiveTintColor: "#9ca3af",
+        tabBarStyle: {
+          backgroundColor: "#111827",
+          borderTopColor: "#374151",
+        },
+      }}
+    >
+      <Tabs.Screen name="dashboard" options={{ title: "Dashboard" }} />
+      <Tabs.Screen name="menu" options={{ title: "Menu" }} />
+      <Tabs.Screen name="pemesanan" options={{ title: "Pemesanan" }} />
+      <Tabs.Screen name="laporan" options={{ title: "Laporan" }} />
     </Tabs>
   );
 }
---
+++ config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDzyS6SLIpvCztJI2B_2u5UXV2V3LXIkS8",
+  authDomain: "dbrestoran-36828.firebaseapp.com",
+  projectId: "dbrestoran-36828",
+  storageBucket: "dbrestoran-36828.firebasestorage.app",
+  messagingSenderId: "599641091348",
+  appId: "1:599641091348:web:b7d7176e622c342d206601",
+  measurementId: "G-GELBHE280S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
